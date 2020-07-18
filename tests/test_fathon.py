07c8772f9dd136a0c68e97e201bfcf0e7c4e2306
Stefano <stefanobianchi9@gmail.com>
2020-07-18 23:32:28 +0200

    test

--- tests/test_fathon.py
+++ tests/test_fathon.py
@@ -1,6 +1,7 @@
 import numpy as np
 import fathon
 import math
+import os
 
 # FUNCTIONALITY TESTS
 # -------------------
@@ -10,18 +11,18 @@ import math
 # - MATLAB code to reproduce the paper results can be found at:
 #   https://it.mathworks.com/matlabcentral/fileexchange/38262-multifractal-detrended-fluctuation-analyses
 
-TESTS_PATH = './tests/'
+TESTS_PATH = os.path.dirname(os.path.abspath(__file__))
 
 #####
 # Time series to test functionality
 # (extracted from Ihlen's MATLAB package "Multifractal detrended fluctuation analyses")
 #####
 # white noise
-wn = np.loadtxt(TESTS_PATH+'mat/whitenoise.txt')
+wn = np.loadtxt(os.path.join(TESTS_PATH, 'mat/whitenoise.txt'))
 # monofractal time series
-mn = np.loadtxt(TESTS_PATH+'mat/monofractal.txt')
+mn = np.loadtxt(os.path.join(TESTS_PATH, 'mat/monofractal.txt'))
 # multifractal time series
-mf = np.loadtxt(TESTS_PATH+'mat/multifractal.txt')
+mf = np.loadtxt(os.path.join(TESTS_PATH, 'mat/multifractal.txt'))
 #####
 
 # defining parameters and scales as in Ihlen's paper
@@ -149,13 +150,13 @@ def test_mat_mfdfa_mn():
 # Time series for regression tests
 #####
 # co2 residuals after yearly cycle
-ts1 = np.loadtxt(TESTS_PATH+'ts1.txt')
+ts1 = np.loadtxt(os.path.join(TESTS_PATH, +'ts1.txt'))
 ts1 = fathon.toAggregated(ts1)
 # other co2 residuals after yearly cycle
-ts2 = np.loadtxt(TESTS_PATH+'ts2.txt')
+ts2 = np.loadtxt(os.path.join(TESTS_PATH, +'ts2.txt'))
 ts2 = fathon.toAggregated(ts2)
 # multifractal data
-ts3 = np.loadtxt(TESTS_PATH+'ts3.txt')
+ts3 = np.loadtxt(os.path.join(TESTS_PATH, +'ts3.txt'))
 ts3 = fathon.toAggregated(ts3)
 #####
 
