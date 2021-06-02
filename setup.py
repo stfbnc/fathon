8d8851da953d06411f85c7a1079aa0a4b01c0712
Stefano <stefanobianchi9@gmail.com>
2021-06-02 10:38:55 +0200

    collapse not for windows

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ if __name__ == "__main__":
         readme_file.close()
 
         setup(name="fathon",
-              version="1.3.post3",
+              version="1.3.post4",
               author="Stefano Bianchi",
               author_email="fathon.package@gmail.com",
               url="https://github.com/stfbnc/fathon.git",
