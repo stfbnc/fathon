f083fa25aa912bd27b7ef1cbd9c7401d4e18f43a
stefano.bianchi <sbianchi@cosmopharma.com>
2022-05-26 10:07:40 +0200

    test

--- upload_wheels.sh
+++ upload_wheels.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-upload_ret=$(twine upload --skip-existing wheelhouse/* -r testpypi -u "$TEST_PYPI_USR" -p "$TEST_PYPI_PWD")
-
-if [ "$upload_ret" = 0 ]; then
-	echo "Succesfully uploaded wheels"
-else
-	echo "Cannot upload wheels, already existing"
+upload_ret=1
+if [ "$PYPI_UPLOAD" = "test" ]; then
+	#upload_ret=$(
+	python3 -m twine upload --skip-existing wheelhouse/* -r testpypi -u "$TEST_PYPI_USR" -p "$TEST_PYPI_PWD" #)
+elif [ "$PYPI_UPLOAD" = "release" ]; then
+	#upload_ret=$(
+	python3 -m twine upload --skip-existing wheelhouse/* -u "$PYPI_USR" -p "$PYPI_PWD" #)
 fi
 
-exit 0
+#if [ "$upload_ret" = 0 ]; then
+#	echo "Succesfully uploaded wheels"
+#else
+#	echo "Cannot upload wheels, already existing"
+#fi
+
+#exit 0
 
