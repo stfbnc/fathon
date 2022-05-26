80a1932a5e6d9f137d942d6464ed4a0d8baad2b2
stefano.bianchi <sbianchi@cosmopharma.com>
2022-05-26 11:52:06 +0200

    upload wheels script refactor

--- upload_wheels.sh
+++ upload_wheels.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-upload_ret=1
 if [ "$PYPI_UPLOAD" = "test" ]; then
-	echo "Uploading on test pypi"
-	upload_ret=$(python3 -m twine upload --skip-existing wheelhouse/* -r testpypi -u "$TEST_PYPI_USR" -p "$TEST_PYPI_PWD")
+	echo "Uploading on Test Pypi"
+	python3 -m twine upload --skip-existing wheelhouse/* -r testpypi -u "$TEST_PYPI_USR" -p "$TEST_PYPI_PWD"
 elif [ "$PYPI_UPLOAD" = "release" ]; then
-	echo "Uploading on pypi"
-	upload_ret=$(python3 -m twine upload --skip-existing wheelhouse/* -u "$PYPI_USR" -p "$PYPI_PWD")
-fi
-
-if [ "$upload_ret" = 0 ]; then
-	echo "Failed to upload wheels"
-else
-	echo "Successfully uploaded wheels"
+	echo "Uploading on Pypi"
+	python3 -m twine upload --skip-existing wheelhouse/* -u "$PYPI_USR" -p "$PYPI_PWD"
 fi
 
 exit 0
