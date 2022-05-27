d040bbcdcd26ab93cfe178e9e92a9f91b7a6041a
stfbnc <stefanobianchi9@gmail.com>
2022-05-27 12:13:24 +0200

    powershell script

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ if __name__ == "__main__":
         readme_file.close()
 
         setup(name="fathon",
-              version="1.3.1.post2",
+              version="1.3.1",
               author="Stefano Bianchi",
               author_email="fathon.package@gmail.com",
               url="https://github.com/stfbnc/fathon.git",
