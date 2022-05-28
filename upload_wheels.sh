a6ab9538e46927bb8c55cf2af87d083495514355
stfbnc <stefanobianchi9@gmail.com>
2022-05-28 12:04:06 +0200

    appveyor upload artifacts

--- upload_wheels.sh
+++ upload_wheels.sh
@@ -2,10 +2,10 @@
 
 if [ "$PYPI_UPLOAD" = "test" ]; then
 	echo "Uploading on Test Pypi"
-	python3 -m twine upload --skip-existing wheelhouse/* -r testpypi -u "$TEST_PYPI_USR" -p "$TEST_PYPI_PWD"
+	python3 -m twine upload --verbose --skip-existing wheelhouse/* -r testpypi -u "$TEST_PYPI_USR" -p "$TEST_PYPI_PWD"
 elif [ "$PYPI_UPLOAD" = "release" ]; then
 	echo "Uploading on Pypi"
-	python3 -m twine upload --skip-existing wheelhouse/* -u "$PYPI_USR" -p "$PYPI_PWD"
+	python3 -m twine upload --verbose --skip-existing wheelhouse/* -u "$PYPI_USR" -p "$PYPI_PWD"
 fi
 
 exit 0
