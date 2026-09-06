9f87dc0bfa2daacb4180b92de4a76155ab0110b7
stfbnc <stefanobianchi9@gmail.com>
2026-09-06 23:18:59 +0200

    update for macos build

--- upload_test_wheels.sh
+++ upload_test_wheels.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Uploading on Test Pypi"
-python3 -m twine upload --verbose --skip-existing wheelhouse/* -r testpypi -u __token__ -p "$TEST_PYPI_TOKEN"
+python -m twine upload --verbose --skip-existing wheelhouse/* -r testpypi -u __token__ -p "$TEST_PYPI_TOKEN"
 
 exit 0
